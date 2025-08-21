3fbc24b73bd22b245074a37060680c5a54ec3adb
juliafg29 <j.gazolla@grad.ufsc.br>
2025-08-21 17:12:53 -0300

    ajustes tranf de viewport

--- Entrega 1_1/reta.py
+++ Entrega 1_1/reta.py
@@ -3,11 +3,13 @@ from tkinter import *
 
 class Reta():
 
-    def __init__(self, canva_desenho, x1, y1, x2, y2):
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
+    def __init__(self, canva_desenho, lista_pontos):
 
+        self.lista_pontos = lista_pontos
+        
         canva_desenho.delete("all")
-        canva_desenho.create_line(self.x1, self.y1, self.x2, self.y2, fill="black", width=2)
+
+        x1, y1 = self.lista_pontos[0]
+        x2, y2 = self.lista_pontos[1]
+            # Desenha reta
+        canva_desenho.create_line(x1, y1, x2, y2, fill="black", width=2)
