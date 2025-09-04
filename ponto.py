72b383153a17e395c4346b660669cdd8c46592a7
juliafg29 <j.gazolla@grad.ufsc.br>
2025-09-04 16:20:33 -0300

    rotacao window para todos objetos

--- ponto.py
+++ ponto.py
@@ -10,7 +10,8 @@ class Ponto(ObjetoGrafico):
         super().__init__(pontos, cor, window)
 
     def desenhar(self, canvas, window, scn, viewport):
-        xw, yw = self.pontos[0]
+        #xw, yw = self.pontos[0]
+        xw, yw = self.rotacao_window(*self.pontos[0])
         xv, yv = scn.world_to_scn_to_viewport(xw, yw, window, viewport)
         raio = 2
         canvas.create_oval(xv-raio, yv-raio, xv+raio, yv+raio, fill=self.cor, outline=self.cor)
--- reta.py
+++ reta.py
@@ -2,7 +2,9 @@ from objeto import ObjetoGrafico
 
 class Reta(ObjetoGrafico):
     def desenhar(self, canvas, window, scn, viewport):
-        (x1, y1), (x2, y2) = self.pontos
+        #(x1, y1), (x2, y2) = self.pontos
+        (x1, y1) = self.rotacao_window(*self.pontos[0])
+        (x2, y2) = self.rotacao_window(*self.pontos[1])
         xv1, yv1 = scn.world_to_scn_to_viewport(x1, y1, window, viewport)
         xv2, yv2 = scn.world_to_scn_to_viewport(x2, y2, window, viewport)
         canvas.create_line(xv1, yv1, xv2, yv2, fill=self.cor, width=2)
